f583a83bb3aee1e3df3d2d8d8fcca1d5375f6b68
Ian Park <Ian Park>
2024-12-03 21:02:10 +0900

    Finish base modifications for site to be useful.

---
+++ astro.config.ts
@@ -0,0 +1,140 @@
+import fs from "node:fs";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import expressiveCode from "astro-expressive-code";
+import icon from "astro-icon";
+import robotsTxt from "astro-robots-txt";
+import webmanifest from "astro-webmanifest";
+import { defineConfig } from "astro/config";
+import { expressiveCodeOptions } from "./src/site.config";
+import { siteConfig } from "./src/site.config";
+
+// Remark plugins
+import remarkDirective from "remark-directive"; /* Handle ::: directives as nodes */
+import remarkUnwrapImages from "remark-unwrap-images";
+import { remarkReadingTime } from "./src/plugins/remark-reading-time";
+
+// Rehype plugins
+import rehypeExternalLinks from "rehype-external-links";
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
+
+
+// https://astro.build/config
+export default defineConfig({
+  image: {
+    domains: ["webmention.io"],
+  },
+  integrations: [
+    expressiveCode(expressiveCodeOptions),
+    icon(),
+    tailwind({
+      applyBaseStyles: false,
+      nesting: true,
+    }),
+    sitemap(),
+    mdx(),
+    robotsTxt(),
+    webmanifest({
+      // See: https://github.com/alextim/astro-lib/blob/main/packages/astro-webmanifest/README.md
+      /**
+       * required
+       **/
+      name: siteConfig.title,
+      /**
+       * optional
+       **/
+      short_name: "Ian's Blog",
+      description: siteConfig.description,
+      lang: siteConfig.lang,
+      icon: "public/icon.png", // the source for generating favicon & icons
+      icons: [
+	{
+	  src: "icons/apple-touch-icon.png", // used in src/components/BaseHead.astro L:26
+	  sizes: "180x180",
+	  type: "image/png",
+	},
+	{
+	  src: "icons/icon-192.png",
+	  sizes: "192x192",
+	  type: "image/png",
+	},
+	{
+	  src: "icons/icon-512.png",
+	  sizes: "512x512",
+	  type: "image/png",
+	},
+      ],
+      start_url: "/",
+      background_color: "#1d1f21",
+      theme_color: "#2bbc8a",
+      display: "standalone",
+      config: {
+	insertFaviconLinks: false,
+	insertThemeColorMeta: false,
+	insertManifestLink: false,
+      },
+    }),
+  ],
+  markdown: {
+    rehypePlugins: [
+      [
+	rehypeExternalLinks,
+	{
+	  rel: ["nofollow, noreferrer"],
+	  target: "_blank",
+	},
+      ],
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'append',
+          content: {
+            type: 'raw',
+            value: '<svg class="h-2 w-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z"/></svg>',
+          },
+          headingProperties: {
+            className: ['anchor'],
+          },
+          properties: {
+            className: ['anchor-link'],
+          },
+        },
+      ],
+    ],
+    remarkPlugins: [remarkUnwrapImages, remarkReadingTime, remarkDirective],
+    remarkRehype: {
+      footnoteLabel: "",
+      footnoteLabelProperties: {
+	className: [""],
+      },
+    },
+  },
+  // https://docs.astro.build/en/guides/prefetch/
+  prefetch: true,
+  site: "https://ianpark.dev/",
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+    plugins: [rawFonts([".ttf", ".woff"])],
+  },
+});
+
+function rawFonts(ext: string[]) {
+  return {
+    name: "vite-plugin-raw-fonts",
+    // @ts-expect-error:next-line
+    transform(_, id) {
+      if (ext.some((e) => id.endsWith(e))) {
+	const buffer = fs.readFileSync(id);
+	return {
+	  code: `export default ${JSON.stringify(buffer)}`,
+	  map: null,
+	};
+      }
+    },
+  };
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,239 @@
+import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
+import plugin from "tailwindcss/plugin";
+
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}",
+    "!./src/pages/og-image/[slug].png.ts",
+  ],
+  corePlugins: {
+    // disable some core plugins as they are included in the css, even when unused
+    borderOpacity: false,
+    fontVariantNumeric: false,
+    ringOffsetColor: false,
+    ringOffsetWidth: false,
+    scrollSnapType: false,
+    textOpacity: false,
+    touchAction: false,
+  },
+  darkMode: ["class", '[data-theme="dark"]'],
+  // darkMode: ['selector', '[data-theme="dark"]'],
+  plugins: [
+    require("@tailwindcss/typography"),
+    plugin(({ addComponents }) => {
+      addComponents({
+	".cactus-link": {
+	  "&:hover": {
+	    "@apply decoration-link decoration-2": {},
+	  },
+	  "@apply underline underline-offset-2": {},
+	},
+	".title": {
+	  "@apply text-2xl font-semibold text-accent-2": {},
+	},
+      });
+    }),
+  ],
+  theme: {
+    // keyframes: {
+
+    // },
+    // animation: {
+    //   slideIn: "slideIn .25s ease-in-out forwards var(--delay, 0)"
+    // }
+
+    extend: {
+      keyframes: {
+        slideIn: {
+          from: {
+            opacity: "0",
+            transform: "translateY(100%)",
+          },
+          to: {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        slideInX: {
+          from: {
+            opacity: "0",
+            transform: "translateX(-30px)",
+          },
+          to: {
+            opacity: "1",
+            transform: "translateX(0)",
+          },
+        },
+        slideiny: {
+          from: {
+            opacity: "0",
+            transform: "translateY(30px)",
+          },
+          to: {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+
+        },
+        slideinx: {
+          from: {
+            opacity: "0",
+            transform: "translateX(-10px)",
+          },
+          to: {
+            opacity: "1",
+            transform: "translateX(0)",
+          },
+
+        },
+        slideinxreverse: {
+          from: {
+            opacity: "0",
+            transform: "translateX(10px)",
+          },
+          to: {
+            opacity: "1",
+            transform: "translateX(0)",
+          },
+
+        },
+        fadeIn: {
+          from: {
+            opacity: "0",
+          },
+          to: {
+            opacity: "1",
+          },
+
+        },
+      },
+      animation: {
+        // slidein: "slidein 1s ease 300ms",
+        // slidein300: "slidein 1s ease 300ms forwards",
+        // slidein500: "slidein 1s ease 500ms forwards",
+        // slidein700: "slidein 1s ease 700ms forwards",
+        // animation: {
+        fadeIn: "fadeIn 0.5s ease forwards var(--fade-delay, 0)",
+        slideIn: "slideIn 0.5s ease forwards var(--delay, 0)",
+        slideInX: "slideInX 0.5s ease forwards var(--delay, 0)",
+        slideiny: "slideiny 1s ease var(--slideiny-delay, 0) forwards",
+        slideinx: "slideinx 1s ease var(--slideinx-delay, 0) forwards",
+        slideinxreverse: "slideinxreverse 1s ease var(--slideinxreverse-delay, 0) forwards",
+      },
+      colors: {
+	accent: "hsl(var(--theme-accent) / <alpha-value>)",
+	"accent-2": "hsl(var(--theme-accent-2) / <alpha-value>)",
+	bgColor: "hsl(var(--theme-bg) / <alpha-value>)",
+	link: "hsl(var(--theme-link) / <alpha-value>)",
+	quote: "hsl(var(--theme-quote) / <alpha-value>)",
+	textColor: "hsl(var(--theme-text) / <alpha-value>)",
+	textColorFaded: "hsl(var(--theme-text-faded) / <alpha-value>)",
+	textColorMoreFaded: "hsl(var(--theme-text-more-faded) / <alpha-value>)",
+      },
+      fontFamily: {
+	// Add any custom fonts here
+	// sans: [...fontFamily.sans],
+	sans: ['Roboto', ...fontFamily.sans],
+	serif: ['Alice', ...fontFamily.serif],
+        mono: ['RobotoMono', ...fontFamily.mono]
+      },
+      transitionProperty: {
+	height: "height",
+      },
+      // @ts-expect-error
+      // Remove above once tailwindcss exposes theme type
+      typography: (theme) => ({
+	DEFAULT: {
+	  css: {
+            // maxWidth: '0%', // add required value here
+	    a: {
+	      "@apply cactus-link": "",
+	    },
+	    blockquote: {
+	      borderLeftWidth: "0",
+	    },
+            code: {
+	      border: "1px solid #666",
+	      borderRadius: "2px",
+              '&::before': {
+                content: '""',
+              },
+              '&::after': {
+                content: '""',
+              },
+            },
+	    kbd: {
+	      "@apply dark:bg-textColor": "",
+	    },
+	    hr: {
+	      borderTopStyle: "solid",
+	    },
+	    strong: {
+	      fontWeight: "700",
+	    },
+	    sup: {
+	      "@apply ms-0.5": "",
+	      a: {
+		"&:after": {
+		  content: "']'",
+		},
+		"&:before": {
+		  content: "'['",
+		},
+		"&:hover": {
+		  "@apply text-link no-underline bg-none": "",
+		},
+		"@apply bg-none": "",
+	      },
+	    },
+	    /* Table */
+	    "tbody tr": {
+	      borderBottomWidth: "none",
+	    },
+	    tfoot: {
+	      borderTop: "1px solid #666",
+	    },
+	    thead: {
+	      borderBottomWidth: "none",
+	    },
+	    "thead th": {
+	      borderBottom: "1px solid #666",
+	      fontWeight: "700",
+	    },
+	    'th[align="center"], td[align="center"]': {
+	      "text-align": "center",
+	    },
+	    'th[align="right"], td[align="right"]': {
+	      "text-align": "right",
+	    },
+	    'th[align="left"], td[align="left"]': {
+	      "text-align": "left",
+	    },
+	  },
+	},
+	baseProse: {
+	  css: {
+	    "--tw-prose-body": theme("colors.textColor / 1"),
+	    "--tw-prose-bold": theme("colors.textColor / 1"),
+	    "--tw-prose-bullets": theme("colors.textColor / 1"),
+	    "--tw-prose-code": theme("colors.textColor / 1"),
+	    "--tw-prose-headings": theme("colors.accent-2 / 1"),
+	    "--tw-prose-hr": "0.5px solid #666",
+	    "--tw-prose-links": theme("colors.textColor / 1"),
+	    "--tw-prose-quotes": theme("colors.quote / 1"),
+	    "--tw-prose-th-borders": "#666",
+	  },
+	},
+	sm: {
+	  css: {
+	    code: {
+	      fontSize: theme("fontSize.sm")[0],
+	      fontWeight: "400",
+	    },
+	  },
+	},
+      }),
+    },
+  },
+} satisfies Config;
